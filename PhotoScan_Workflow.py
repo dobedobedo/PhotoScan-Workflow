b0c3d677e584508d82ed98090ce5ec1adea1115e
Yu-Hsuan Tu <yuhsuan.tu@uqconnect.edu.au>
2019-01-11 13:54:22 +1000

    Add files via upload

---
+++ PhotoScan_Workflow.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 13:21:23 2017
+
+@author: Yu-Hsuan Tu
+
+This Python Script is developed for Agisoft PhotoScan (current MetaShape) 1.3.4
+Python core is 3.5.2
+
+11 January 2019 Update: Add compatibility of MetaShape 1.5.0
+Update: Add compatibility of PhotoScan 1.4.0
+
+This script runs through all chunks and will do the following:
+    1. Align Photos if there's no tie point
+    2. Do the standard process if there is tie point
+
+When aligning photos, users can decide whether using image quality to disable bad photos
+
+GCP needs to be marked manually
+
+Prerequisites for standard workflow:
+    1. Set CRS
+    2. Photo alignment
+    3. Marking GCP
+    4. Optimse Camera
+    5. Set Region
+
+The standard workflow includes:
+    Build dense point cloud
+    Point cloud classification
+    Build model
+    Build DSM
+    Build DEM
+    Build orthomosaic
+    
+All chunks will be applied.
+The DEM will be generated in duplicated chunk: "chunk name"_DEM respectively
+Therefore, please avoid "_DEM" in your chunk name. Otherwise, it will not be processed.
+"""
+try:
+    import Metashape as PhotoScan
+except ImportError:
+    import PhotoScan
+
+doc = PhotoScan.app.document
+
+#######################################################
+# User variables
+#
+# Variables for image quality filter
+# QualityFilter: True, False
+# QualityCriteria: float number range from 0 to 1 (default 0.5)
+QualityFilter = True
+QualityCriteria = 0.5
+#
+# Variables for photo alignment
+# Accuracy: HighestAccuracy, HighAccuracy, MediumAccuracy, LowAccuracy, LowestAccuracy
+Accuracy = PhotoScan.Accuracy.HighAccuracy
+Key_Limit = 40000
+Tie_Limit = 10000
+#
+# Variables for building dense cloud
+# Quality: UltraQuality, HighQuality, MediumQuality, LowQuality, LowestQuality
+# Filter: AggressiveFiltering, ModerateFiltering, MildFiltering, NoFiltering
+Quality = PhotoScan.Quality.HighQuality
+FilterMode = PhotoScan.FilterMode.MildFiltering
+#
+# Variables for dense cloud ground point classification
+# Maximum distance is usually twice of image resolution
+# Which will be calculated later
+Max_Angle = 13
+Cell_Size = 10
+#
+# Variable for building orthomosaic
+# Since 1.4.0, users can choose performing color correction (vignetting) and balance separately.
+# Blending: AverageBlending, MosaicBlending, MinBlending, MaxBlending, DisabledBlending
+# Color_correction: True, False
+# Color_balance: True, False
+BlendingMode = PhotoScan.BlendingMode.MosaicBlending
+Color_correction = True
+Color_balance = False
+#
+#######################################################
+
+wgs_84 = PhotoScan.CoordinateSystem("EPSG::4326")
+
+def AlignPhoto(chunk, Accuracy, Key_Limit, Tie_Limit, QualityFilter, QualityCriteria):
+    if QualityFilter:
+        if chunk.cameras[0].meta['Image/Quality'] is None:
+            chunk.estimateImageQuality()
+        for band in [band for camera in chunk.cameras for band in camera.planes]:
+            if float(band.meta['Image/Quality']) < QualityCriteria:
+                band.enabled = False
+    chunk.matchPhotos(accuracy=Accuracy, 
+                      generic_preselection=True, 
+                      reference_preselection=True, 
+                      filter_mask=False, 
+                      keypoint_limit=Key_Limit, 
+                      tiepoint_limit=Tie_Limit)
+    chunk.alignCameras(adaptive_fitting=True)
+    
+def BuildDenseCloud(chunk, Quality, FilterMode):
+    try:
+        chunk.buildDenseCloud(quality=Quality, 
+                              filter= FilterMode, 
+                              keep_depth=False, 
+                              reuse_depth=False)
+    except:
+        chunk.buildDepthMaps(quality=Quality,
+                             filter=FilterMode,
+                             reuse_depth=False)
+        chunk.buildDenseCloud(point_colors=True)
+    
+def ClassifyGround(chunk, Max_Angle, Cell_Size):
+    DEM_resolution, Image_resolution = GetResolution(chunk)
+    chunk.dense_cloud.classifyGroundPoints(max_angle=Max_Angle, 
+                                           max_distance=2*Image_resolution, 
+                                           cell_size=Cell_Size)
+    
+def BuildModel(chunk):
+    chunk.buildModel(surface=PhotoScan.SurfaceType.HeightField, 
+                     interpolation=PhotoScan.Interpolation.EnabledInterpolation, 
+                     face_count=PhotoScan.FaceCount.HighFaceCount, 
+                     source=PhotoScan.DataSource.DenseCloudData, 
+                     vertex_colors=True)
+    
+def BuildDSM(chunk):
+    try:
+        chunk.buildDem(source=PhotoScan.DataSource.DenseCloudData, 
+                       interpolation=PhotoScan.Interpolation.EnabledInterpolation, 
+                       projection = chunk.crs)
+    except:
+        chunk.buildDem(source=PhotoScan.DataSource.DenseCloudData, 
+                       interpolation=PhotoScan.Interpolation.EnabledInterpolation)
+
+def BuildDEM(chunk):
+    try:
+        chunk.buildDem(source=PhotoScan.DataSource.DenseCloudData, 
+                       interpolation=PhotoScan.Interpolation.EnabledInterpolation, 
+                       projection = chunk.crs,
+                       classes=[PhotoScan.PointClass.Ground])
+    except:
+        chunk.buildDem(source=PhotoScan.DataSource.DenseCloudData, 
+                       interpolation=PhotoScan.Interpolation.EnabledInterpolation, 
+                       classes=[PhotoScan.PointClass.Ground])
+    
+def BuildMosaic(chunk, BlendingMode):
+    try:
+        chunk.buildOrthomosaic(surface=PhotoScan.DataSource.ElevationData, 
+                               blending=BlendingMode, 
+                               color_correction=Color_correction, 
+                               fill_holes=True, 
+                               projection= chunk.crs)
+    except:
+        if Color_correction:
+            chunk.calibrateColors(source_data=PhotoScan.DataSource.ModelData, color_balance=Color_balance)
+        chunk.buildOrthomosaic(surface=PhotoScan.DataSource.ElevationData, 
+                               blending=BlendingMode,  
+                               fill_holes=True)
+    
+def StandardWorkflow(doc, chunk, **kwargs):
+    doc.save()
+    
+    # Skip the chunk if it is the DEM chunk we created
+    if '_DEM' in chunk.label:
+        pass
+    else:
+        if chunk.dense_cloud is None:
+            BuildDenseCloud(chunk, kwargs['Quality'], kwargs['FilterMode'])
+    # Must save before classification. Otherwise it fails.
+            doc.save()
+            ClassifyGround(chunk, kwargs['Max_Angle'], kwargs['Cell_Size'])
+            doc.save()
+        if chunk.model is None:
+            BuildModel(chunk)
+        doc.save()
+        
+        if chunk.elevation is None:
+            BuildDSM(chunk)
+        
+    # Because each chunk can only contain one elevation data
+    # Therefore, we need to duplicate the chunk to create DEM
+            new_chunk = chunk.copy(items=[PhotoScan.DataSource.DenseCloudData])
+            new_chunk.label = chunk.label + '_DEM'
+            doc.save()
+            BuildDEM(new_chunk)
+        doc.save()
+        
+    # Change the active chunk back
+        doc.chunk = chunk
+        
+        if chunk.orthomosaic is None:
+            BuildMosaic(chunk, kwargs['BlendingMode'])
+        doc.save()
+
+def GetResolution(chunk):
+    DEM_resolution = float(chunk.dense_cloud.meta['dense_cloud/resolution']) * chunk.transform.scale
+    Image_resolution = DEM_resolution / int(chunk.dense_cloud.meta['dense_cloud/depth_downscale'])
+    return DEM_resolution, Image_resolution
+
+# The following process will only be executed when running script    
+if __name__ == '__main__':
+    # Initialising listing chunks
+    chunk_list = doc.chunks
+        
+    # Loop for all initial chunks
+    for chunk in chunk_list:
+        doc.chunk = chunk
+        
+    # Align Photo only if it is not done yet
+        if chunk.point_cloud is None:
+            AlignPhoto(chunk, Accuracy, Key_Limit, Tie_Limit)
+            
+    # Do the rest when there's tie point
+        else:
+            StandardWorkflow(doc, chunk, 
+                             Quality=Quality, FilterMode=FilterMode, 
+                             Max_Angle=Max_Angle, Cell_Size=Cell_Size, 
+                             BlendingMode=BlendingMode)
+    
---
+++ README.md
@@ -0,0 +1,11 @@
+# PhotoScan-Workflow
+Python Script for automation in Agisoft MetaShape (previous PhotoScan) Pro
+  
+# Usage  
+This script will process all chunks with the following behaviour:  
+- **If Photo Alignment is not done yet, align the photos**  
+- **If there is tie point, do the rest workflow**  
+  
+**Note:** The GCP must be marked manually, following camera optimisation  
+You can change some variables in the script for specific purposes  
+Run the script in Tools > Run script
